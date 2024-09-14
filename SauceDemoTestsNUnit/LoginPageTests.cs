5ef6f48855efa0f0e39bc772c591230e5562c29e
James Smith <kaleburdev@gmail.com>
2024-09-14 12:59:17 -0400

    Add a test to see if login page properly redirects after successful login

--- SauceDemoTestsNUnit/LoginPageTests.cs
+++ SauceDemoTestsNUnit/LoginPageTests.cs
@@ -16,11 +16,28 @@ namespace SauceDemoTestsNUnit
         }
 
         [TestCase("standard_user")]
+        [TestCase("problem_user")]
+        [TestCase("performance_glitch_user")]
+        [TestCase("error_user")]
+        [TestCase("visual_user")]
         public void LoginPage_RedirectsToAProductsPage_WhenCorrectlyLoggingIn(string username)
         {
             var password = "secret_sauce";
 
+            _driver.Navigate().GoToUrl(_loginPage.Url);
+            _loginPage.UsernameTextbox.SendKeys(username);
+            _loginPage.PasswordTextbox.SendKeys(password);
+            _loginPage.LoginButton.Click();
+            var productsTitleVisible = _loginPage.ProductsTitle.Displayed;
 
+            Assert.IsTrue(productsTitleVisible);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _driver.Quit();
+            _driver.Dispose();
         }
     }
 }
